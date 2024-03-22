1f96505dea2a0c9dec1868319365f11274ca4601
mennarihan5 <menna1986@live.com>
2024-03-22 23:03:52 +0200

    Doing Login page CSS

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -10,53 +10,54 @@ function LoginPage() {
     return(
         <div className={styles.loginPage}>
             <div className={styles.header}>
-                <Logo />
+                <Logo className={styles.logo}/>
             </div>
-            <div className={styles.loginForm}>
-                <div className={styles.continue}>
-                    <Button onClick={()=>{}} type="primary" className={styles.facebookBtn}>
-                        <FaFacebookF className={styles.facebook}/>
-                        <p className={styles.btnText}>Continue with Facebook</p>
-                    </Button>
-                    <Button onClick={()=>{}} type="primary" className={styles.appleBtn}>
-                         <FaApple />
-                         <p className={styles.btnText}>Continue with Apple</p>
-                    </Button>
-                    <Button onClick={()=>{}} type="secondary" className={styles.googleBtn}>
-                        <FcGoogle />
-                        <p className={`${styles.btnText} ${styles.googleText}`}>Continue with Google</p>
-                    </Button>
-                </div>
-                <div className={styles.divider}>
-                    <span>OR</span>
-                </div>
-                <div className={styles.form}>
-                    <label for={styles.username}>Email address or username</label><br/>
-                    <input type='text' id={styles.username} placeholder='Email address or username'/> <br/>
-                    <label for={styles.password}>Password</label><br/>
-                    <div className={styles.passwordWrapper}>
-                        <input type='password' id={styles.password} placeholder='Password'/>
-                        <BiHide className={styles.hide}/>
-                    </div><br/>
-                    <p><a href="#/forgotPassword">Forgot your password?</a></p> <br/>
-                    <div className={styles.login}>
-                        <div className={styles.rememberMe}>
-                            <input type='checkbox' id={styles.remember}/>
-                            <label for={styles.remember}>Remember me</label>
+            <div className={styles.loginFormWrapper}>
+                <div className={styles.loginForm}>
+                    <div className={styles.continue}>
+                        <Button onClick={()=>{}} type="primary" className={styles.facebookBtn}>
+                            <FaFacebookF className={styles.facebookIcon}/>
+                            <p className={styles.btnText}>Continue with Facebook</p>
+                        </Button>
+                        <Button onClick={()=>{}} type="primary" className={styles.appleBtn}>
+                            <FaApple className={styles.appleIcon} />
+                            <p className={styles.btnText}>Continue with Apple</p>
+                        </Button>
+                        <Button onClick={()=>{}} type="secondary" className={styles.googleBtn}>
+                            <FcGoogle className={styles.googleIcon} />
+                            <p className={`${styles.btnText} ${styles.googleText}`}>Continue with Google</p>
+                        </Button>
+                    </div>
+                    <div className={styles.divider}>
+                        <span>OR</span>
+                    </div>
+                    <div className={styles.form}>
+                        <label for={styles.username}>Email address or username</label><br/>
+                        <input type='text' id={styles.username} placeholder='Email address or username'/> <br/>
+                        <label for={styles.password}>Password</label><br/>
+                        <div className={styles.passwordWrapper}>
+                            <input type='password' id={styles.password} placeholder='Password'/>
+                            <BiHide className={styles.hide}/>
+                        </div><br/>
+                        <p><a href="#/forgotPassword">Forgot your password?</a></p> <br/>
+                        <div className={styles.login}>
+                            <div className={styles.rememberMe}>
+                                <input type='checkbox' id={styles.remember}/>
+                                <label for={styles.remember}>Remember me</label>
+                            </div>
+                            <Button className={styles.loginBtn} onClick={()=>{}} type="primary">
+                                log in
+                            </Button>
                         </div>
-                        <Button className={styles.loginBtn} onClick={()=>{}} type="primary">
-                            log in
+                    </div>
+                    <div className={styles.formFooter}>
+                        <h3 className={styles.signupTxt}>Don't have an account?</h3>
+                        <Button className={styles.signupBtn} onClick={()=>{}} type="secondary">
+                            Sign up for Spotify
                         </Button>
                     </div>
                 </div>
-                <div className={styles.formFooter}>
-                    <h3 className={styles.signupTxt}>Don't have an account?</h3>
-                    <Button className={styles.signupBtn} onClick={()=>{}} type="secondary">
-                        Sign up for Spotify
-                    </Button>
-                </div>
             </div>
-
         </div>
 
     )
